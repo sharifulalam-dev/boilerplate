2fa3e28ca81b1b8c95ee7561fd8c2e6fdf59a090
sharifulalam-dev <156599580+sharifulalam-dev@users.noreply.github.com>
2024-12-11 02:35:34 +0600

    Update router.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element:<div className="font-bold flex justify-center items-center text-3xl bg-blue-600  text-white min-h-20">
+        Hello world!
+      </div>,
   },
 ]);
 
